fda0d54c606a8c3a05b724c6be1e428f635b4c1d
kunal <kunalgarg054@gmail.com>
2024-04-16 20:46:33 +0530

    config react-router-dom and added basic routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Homepage } from './pages/Homepage';
+import { Signin } from './pages/Signin';
+import { Signup } from './pages/Signup';
+import { Services } from './pages/Services';
+import { Partners } from './pages/Partners';
+
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />}/>
+        <Route path='/signin' element={<Signin />}/>
+        <Route path='/signup' element={<Signup />}/>
+        <Route path='/services' element={<Services />}/>
+        <Route path='/partners' element={<Partners />}/>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
