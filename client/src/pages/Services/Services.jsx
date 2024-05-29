dd3ef9dc42b119d344ea9f4a878eead8be48f098
Akangkha <akangkhasarkar@gmail.com>
2024-05-29 14:26:24 +0530

    modified Services

--- client/src/pages/Services/Services.jsx
+++ client/src/pages/Services/Services.jsx
@@ -29,7 +29,7 @@ export const Services = () => {
   }, []);
 
   return (
-    <div className="w-full flex flex-col  items-center justify-center bg-gray-200">
+    <div className="w-full flex flex-col  items-center justify-center bg-gray-300">
       <Navbar />
       <div className="w-full h-20 text-base flex items-center ">
         <p className="mx-24">
@@ -40,26 +40,37 @@ export const Services = () => {
         </p>
       </div>
       <div
-        className="w-full h-44  items-center flex"
+        className="w-full h-[60vh] flex items-center justify-center relative"
         style={{
-          background: "url('/images/header.webp')",
+          background:
+            "url(/images/hero/serviceHero.jpg) no-repeat center center fixed",
           backgroundSize: "cover",
         }}
       >
-        <div className="mx-24">
-          <h1 className="text-4xl font-bold">Categories</h1>
-          <p>This is the content for categories.</p>
+        <div className="absolute inset-0 bg-black opacity-50"></div>{" "}
+        <div className="relative text-center text-white mx-24">
+          {" "}
+          <h1 className="text-4xl py-8 font-bold">Expertise and Solutions</h1>
+          <p>
+            Unlock the extraordinary with our unparalleled services. At the
+            intersection of innovation and excellence, we deliver solutions that
+            transform visions into reality. Dive into our curated suite of
+            offerings, each crafted to power your success and set you apart from
+            the rest. Experience the difference with us – where your ambitions
+            meet our expertise..
+          </p>
         </div>
       </div>
-      <div className="w-full  text-base flex items-center justify-between my-16 md:mb-24 flex-col md:flex-row ">
-        <p className="mx-24 text-center">
+
+      <div className="w-full  text-base flex items-center justify-between mt-8 flex-col md:flex-row ">
+        <p className=" text-center mx-12">
           {" "}
           Showing results for {Object.keys(categorizedData).length} categories
         </p>
       </div>
-      <div className="flex w-full xl:justify-normal gap-12 mb-32 flex-wrap justify-center sm:justify-normal">
+      <div className="flex w-full xl:justify-normal gap-12 mb-32 flex-wrap justify-center sm:justify-normal ">
         {Object.keys(categorizedData).map((category, index) => (
-          <div key={index} className="flex">
+          <div key={index} className="flex flex-wrap justify-center sm:justify-normal ">
             <Card
               image={categorizedData[category][0].image}
               category={categorizedData[category][0].category}
@@ -73,6 +84,17 @@ export const Services = () => {
                 category={categorizedData[category][1].category}
                 title={categorizedData[category][1].title}
                 profile={categorizedData[category][1].profile}
+                amount=""
+              />
+            ) : (
+              ""
+            )}
+            {categorizedData[category].length > 2 ? (
+              <Card
+                image={categorizedData[category][2].image}
+                category={categorizedData[category][2].category}
+                title={categorizedData[category][2].title}
+                profile={categorizedData[category][2].profile}
                 amount=""
               />
             ) : (
